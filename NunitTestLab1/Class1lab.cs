f0425a563bd69aadf23924347920f2a22137400e
vboopathy <vboopathy@lixar.com>
2023-02-21 13:56:41 -0500

    Add project files.

---
+++ NunitTestLab1/Class1lab.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitTestLab1
+{
+    internal class Class1lab
+    {
+        public static void Main()
+        {
+            int firstAngle = 0, secondAngle = 0, thirdAngle = 0;
+            string customTriangle;
+
+            Console.Write("\n\n");
+            Console.Write("Check that the triangle is formed or not?\n");
+            Console.Write("-------------------------------------------");
+
+            Console.WriteLine("Enter the first angle");
+            while (!int.TryParse(Console.ReadLine(), out firstAngle))
+            {
+                Console.WriteLine("Enter valid first angle\n");
+            }
+
+            Console.WriteLine("Enter the second angle");
+            while (!int.TryParse(Console.ReadLine(), out secondAngle))
+            {
+                Console.WriteLine("Enter valid second angle\n");
+            }
+
+            Console.WriteLine("Enter the third angle");
+            while (!int.TryParse(Console.ReadLine(), out thirdAngle))
+            {
+                Console.WriteLine("Enter valid third angle\n");
+            }
+
+            customTriangle = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+            Console.WriteLine(customTriangle);
+            Console.ReadLine();
+        }
+    }
+
+}
+
---
+++ testdata/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NunitTestLab1;
+
+namespace testdata
+{
+    public class Class1
+    {
+        [TestFixture]
+        public class Class2
+        {
+            //Sonia Jayashankar Assignment
+            [Test]
+            public void ValidTriangle_Input60and60_OutputValidTriangle()
+            {
+                //Arrange
+                int firstAngle = 60;
+                int secondAngle = 60;
+                int thirdAngle = 60;
+                string expected = "The triangle is valid.";
+                //Act   
+                string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+                //Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            [Test]
+            public void ValidTriangle_Input50and50_OutputValidTriangle()
+            {
+                //Arrange
+                int firstAngle = 50;
+                int secondAngle = 50;
+                int thirdAngle = 70;
+                string expected = "The triangle is valid.";
+                //Act   
+                string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+                //Assert
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+    }
+}
